c84d795ac7d897633631e7072c61062760b9caa8
dev.break.Boy.Linkin(Style:🤸‍♂️,🧑‍💻,🎶) <196859099+Pablo-German-Jimenez@users.noreply.github.com>
2026-06-14 21:31:42 -0300

    login,sidebar and main routes done

--- src/context/providers/AuthContext.tsx
+++ src/context/providers/AuthContext.tsx
@@ -13,7 +13,6 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-  const SkinnerUser = null;
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
 
@@ -42,7 +41,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 export function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error("useAuth debe ser usado dentro de un AuthProvider");
+    throw new Error("useAuth debe ser usado dentro de un AuthProvider pro");
   }
   return context;
 }
